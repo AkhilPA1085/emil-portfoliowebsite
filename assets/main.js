fa5e110c2526267e5d1b10582ebb053bc2e5f737
Akhil P A <akhilpa1085@gamil.com>
2023-06-19 14:56:44 +0530

    mobile view edited,animations added

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-// navbar
 $(document).ready(function() {
+  // navbar
   var prevScrollpos = $(window).scrollTop();
     $(window).on("scroll", function() {
       var currentScrollPos = $(window).scrollTop();
@@ -9,45 +9,45 @@ $(document).ready(function() {
         $("#header").css("top", "-100vh");
       }
       prevScrollpos = currentScrollPos;
+  });
+    
+  $('.navbar-toggler').on('click',function(){
+      $('i').toggleClass('bi-list bi-x-lg');
+  })
+    
+  // navlink
+  $('.nav-link').click(function() {
+      $('.nav-link').removeClass('active');
+      $(this).addClass('active');
+  });
+ 
+
+  // porfolio
+  var firstFilter = $('[data-filter]').first();
+  firstFilter.addClass("active");
+  var firstFilterData = firstFilter.data().filter;
+  
+  var filter = firstFilterData;
+  
+  
+    $("[data-filter]").on("click", function() {
+      filter = $(this).attr("data-filter");
+  
+      $("[data-filter]").removeClass("active");
+      $(this).addClass("active");
+      console.log(filter);
+      filterItems();
     });
-    
-    $('.navbar-toggler').on('click',function(){
-        $('i').toggleClass('bi-list bi-x-lg');
-    })
-    
-        
-    $('.nav-link').click(function() {
-        $('.nav-link').removeClass('active');
-        $(this).addClass('active');
-      });
-
-
-      // porfolio
-      var firstFilter = $('[data-filter]').first();
-      firstFilter.addClass("active");
-      var firstFilterData = firstFilter.data().filter;
-      
-      var filter = firstFilterData;
-      
-      
-        $("[data-filter]").on("click", function() {
-          filter = $(this).attr("data-filter");
-      
-          $("[data-filter]").removeClass("active");
-          $(this).addClass("active");
-          console.log(filter);
-          filterItems();
-        });
-      
-        filterItems();
-      
-        function filterItems() {
-          if (filter === firstFilterData) {
-            $(".filter-item").hide();
-            $(".filter-item[data-filter='" + filter + "']").show();
-          } else {
-            $(".filter-item").hide();
-            $(".filter-item[data-filter='" + filter + "']").show();
-          }
-        }
+  
+    filterItems();
+  
+    function filterItems() {
+      if (filter === firstFilterData) {
+        $(".filter-item").hide();
+        $(".filter-item[data-filter='" + filter + "']").show();
+      } else {
+        $(".filter-item").hide();
+        $(".filter-item[data-filter='" + filter + "']").show();
+      }
+    }
 })
